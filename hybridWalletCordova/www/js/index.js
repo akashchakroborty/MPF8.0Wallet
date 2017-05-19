b31be96e32bf5edc2c8ac727c85c1b21a97328cc
akashchakroborty <akashranjanchakroborty@gmail.com>
2017-05-19 20:42:55 +0530

    Version 1.0

---
+++ hybridWalletCordova/www/js/index.js
@@ -0,0 +1,9 @@
+
+function wlCommonInit(){
+
+        console.log('---> wlCommonInit called');
+        var wlevent = new CustomEvent('MFPinit');
+        console.log('---> dispatching MFPinit event');
+        document.dispatchEvent(wlevent);
+
+}
